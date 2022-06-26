1ac1406def9a53dbe3a216bf86ae1916fc8901b0
yourmaingotoguy <khalid.n.mohamed@outlook.com>
2022-06-26 14:33:41 -0400

    Day 2 Part 2 completed

--- src/main/java/com/adventofcode/day2/Dive.java
+++ src/main/java/com/adventofcode/day2/Dive.java
@@ -37,11 +37,41 @@ public class Dive {
         return posX * posY;
     }
 
+    public Integer partTwo() throws IOException, URISyntaxException {
+        List<String> listOfCommands = fileProcessor.processListOfStrings(file);
+        Integer aim = 0;
+        Integer posX = 0;
+        Integer posY = 0;
+
+        for(String command: listOfCommands){
+            String[] commandSplit = command.split("\\s");
+            String direction = commandSplit[0];
+            Integer movement = Integer.parseInt(commandSplit[1]);
+
+            switch (direction.toLowerCase()){
+                case "forward":
+                    posX += movement;
+                    posY += aim*movement;
+                    break;
+                case "up":
+                    aim -= movement;
+                    break;
+                case "down":
+                    aim += movement;
+                    break;
+                default:
+                    throw new RuntimeException("Unexpected token");
+            }
+        }
+
+        return posX * posY;
+    }
+
     public static void main(String[] args) {
         Dive dive = new Dive();
 
         try {
-            Integer res = dive.partOne();
+            Integer res = dive.partTwo();
             System.out.println(res);
 
         } catch (Exception e) {
