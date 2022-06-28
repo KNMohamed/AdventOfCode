3d8ca2346f37e90892b9fa4934c39220573c11b0
yourmaingotoguy <khalid.n.mohamed@outlook.com>
2022-06-28 10:48:01 -0400

    Day 3 part 2 solved

--- src/main/java/com/adventofcode/day3/BinaryDiagnostic.java
+++ src/main/java/com/adventofcode/day3/BinaryDiagnostic.java
@@ -23,7 +23,6 @@ public class BinaryDiagnostic {
      * @return
      */
     public static long frequencyOfOneInBitPositionN(List<String> binaries, int n){
-        int count = 0;
         return binaries.stream().filter(binary -> binary.charAt(n) == '1').count();
     }
 
@@ -34,24 +33,24 @@ public class BinaryDiagnostic {
      * @return
      */
     public static long frequencyOfZeroInBitPositionN(List<String> binaries, int n){
-        int count = 0;
         return binaries.stream().filter(binary -> binary.charAt(n) == '0').count();
     }
 
     /**
      * returns the most frequent bit, given a bit position n, and a list of binary numbers
+     * Note: if frequency of 0 bit and 1 bit is the same return 1
      * @param binaries is the list of binaries from the input file
      * @param n is the bit position to consider
      * @return
      */
     public static char getMostFrequentInBitPositionN(List<String> binaries, int n) {
-        return frequencyOfOneInBitPositionN(binaries,n) > frequencyOfZeroInBitPositionN(binaries,n) ? '1' : '0';
+        return frequencyOfZeroInBitPositionN(binaries,n) > frequencyOfOneInBitPositionN(binaries,n) ? '0' : '1';
     }
 
     /**
      * get the GammaRate by finding the most common bit in the corresponding position of all numbers in the diagnostic report
      * Time Complexity O(n*k) where, n is the number of lines in the diagnostic report; k is the length of the binary numbers
-     * @param binaries
+     * @param binaries is the list of binaries from the input file
      * @return
      */
     public static String getGammaRate(List<String> binaries) {
@@ -66,9 +65,9 @@ public class BinaryDiagnostic {
     }
 
     /**
-     * Helper function to invert binaries (e.x - Epsilon is the invert of Gamma; CO2 scrubber rating is invert of oxygen generator rating);
-     * @param binary
-     * @return
+     * Helper function to invert binaries (e.x - Epsilon is the invert of Gamma);
+     * @param binary is the binary to invert
+     * @return a string representation of the inverted binary
      */
     public static String invertBinary(String binary){
         return binary.replace('0','x').replace('1','0').replace('x','1');
@@ -77,7 +76,7 @@ public class BinaryDiagnostic {
     /**
      * Calculate the power consumption given a list of binaries using generated gamma and epsilon binaries
      * Time Complexity O(n*k) where, n is the number of lines in the diagnostic report; k is the length of the binary numbers
-     * @param binaries
+     * @param binaries is the list of binaries from the input file
      * @return
      */
     public static Integer calculatePowerConsumption(List<String> binaries){
@@ -89,6 +88,52 @@ public class BinaryDiagnostic {
         return powerConsumption;
     }
 
+    /**
+     * oxygen generator rating determined by iteratively finding the most common value at each bit position while removing the least common from the list
+     * @param binaries is the list of binaries from the input file
+     * @return
+     */
+    public static Integer getOxygenRating(List<String> binaries) {
+        List<String> oxygenRating = new ArrayList<>(binaries);
+        int bitLength = oxygenRating.get(0).length();
+        for(int i = 0; i < bitLength; i++){
+            final int bitPos = i;
+            char bit = getMostFrequentInBitPositionN(oxygenRating,i);
+            oxygenRating.removeIf(binary -> binary.charAt(bitPos) != bit);
+            if(oxygenRating.size() == 1) break;
+        }
+
+        return Integer.parseInt(oxygenRating.get(0),2);
+    }
+
+    /**
+     * CO2 scrubber rating determined by applying inverse logic to the oxygen generator rating
+     * @param binaries is the list of binaries from the input file
+     * @return
+     */
+    public static Integer getCo2Rating(List<String> binaries) {
+        List<String> co2Rating = new ArrayList<>(binaries);
+        int bitLength = co2Rating.get(0).length();
+        for(int i = 0; i < bitLength; i++){
+            final int bitPos = i;
+            char bit = getMostFrequentInBitPositionN(co2Rating,i);
+            co2Rating.removeIf(binary -> binary.charAt(bitPos) == bit);
+            if(co2Rating.size() == 1) break;
+        }
+
+        return Integer.parseInt(co2Rating.get(0),2);
+    }
+
+    /**
+     * Life support rating determined by multiplying the CO2 scrubber rating by the oxygen generator rating
+     * @param binaries
+     * @return
+     */
+    public static Integer getLifeSupportRating(List<String> binaries) {
+        return getCo2Rating(binaries) * getOxygenRating(binaries);
+    }
+
+
     public static void main(String[] args) {
        List<String> binaries = null;
         try {
@@ -98,8 +143,10 @@ public class BinaryDiagnostic {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        Integer powerConsumption = BinaryDiagnostic.calculatePowerConsumption(binaries);
-        System.out.println("Power consumption is: " + powerConsumption);
+        if(binaries != null){
+            Integer lifeSupportRating = getLifeSupportRating(binaries);
+            System.out.println(lifeSupportRating);
+        }
     }
 
 }
